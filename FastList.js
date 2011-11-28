e5f815529e7437d1d2ec026463f23b6d594bd0c5
Mikayel Aghasyan <mikayel.aghasyan@gmail.com>
2011-11-29 02:26:07 +0400

    Switched to still empty Ext.ux.FastList

---
+++ FastList.js
@@ -0,0 +1,11 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: mikayelaghasyan
+ * Date: 11/29/11
+ * Time: 2:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+Ext.namespace('Ext.ux');
+
+Ext.ux.FastList = Ext.extend(Ext.List, {
+});
